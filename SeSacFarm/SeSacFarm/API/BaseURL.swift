c6bb5f45dc985f05c1e06ea9805f25507250a65b
kimchanhoo <cksgn0709@gmail.com>
2022-01-08 14:28:54 +0900

    gitignore

---
+++ SeSacFarm/SeSacFarm/API/BaseURL.swift
@@ -0,0 +1,12 @@
+//
+//  BaseURL.swift
+//  SeSacFarm
+//
+//  Created by Hoo's MacBookPro on 2022/01/08.
+//
+
+import Foundation
+
+extension URL {
+	static let baseURL = "http://test.monocoding.com:1231"
+}
